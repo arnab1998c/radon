b9dd9db4a8aa2e34f151dec4c35a3b843900dd96
arnab1998c <arnab1998c@gmail.com>
2022-06-05 21:48:19 +0530

    post and get api books name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://arnab1998c:cQQsemOdnH9UpNog@cluster0.5it9s.mongodb.net/arnab1998c-db?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
